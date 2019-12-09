3e58c258e34c401fdad1f531d21587960c1bcc2c
Marcus <marcus.casey@outlook.com>
2019-12-08 17:41:56 -0800

    added menu functionality

--- UI/src/driver.cpp
+++ UI/src/driver.cpp
@@ -1,6 +1,85 @@
+#include <iostream>
+
+using namespace std;
+
 void ProcessImage();
 
-int main(int argc, char *argv[]) {
-  ProcessImage();
-  
+void MainMenu();
+
+int main(int argc, char *argv[])
+{
+  MainMenu();
+
+  return 0;
+}
+
+void MainMenu()
+{
+
+  char selection;
+
+  cout << "\n Menu";
+
+  cout << "\n========";
+
+  cout << "\n 1 - Load an image for analysis";
+
+  cout << "\n 2 - Check for gravitational lensing";
+
+  cout << "\n 3 - Save modified image";
+
+  cout << "\n 4 - Exit";
+
+  cout << "\n Enter selection: ";
+
+  // read the input
+
+  cin >> selection;
+
+  string fileName;
+  switch (selection)
+
+  {
+
+  case '1':
+  {
+    cout << "\n Enter image name + file type: ";
+    cin >> fileName;
+    cout << "\n File Loaded!\n";
+    //LoadImage();
+    MainMenu();
+  }
+
+  case '2':
+  {
+    cout << "\n Running Detection Algorithm....";
+
+    ProcessImage();
+
+    cout << "\n Complete!";
+
+    MainMenu();
+  }
+
+  case '3':
+  {
+    cout << "\n Image Saved To Base Folder";
+
+    //SaveImage();
+    MainMenu();
+  }
+
+  case '4':
+  {
+    cout << "\n Exiting...";
+  }
+  break;
+
+  default:
+    cout << "\n Invalid selection";
+
+    // no break in the default case
+  }
+
+  cout << "\n";
 }
