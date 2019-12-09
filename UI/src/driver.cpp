d2139b3cb7da76be29fd5f5f4437aee66f5bfb5a
Fireparrot <nignatyuk@gmail.com>
2019-12-08 23:22:16 -0800

    Lotsa changes

--- UI/src/driver.cpp
+++ UI/src/driver.cpp
@@ -1,85 +1,60 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 void ProcessImage();
-
 void MainMenu();
 
-int main(int argc, char *argv[])
-{
-  MainMenu();
-
-  return 0;
+int main(int argc, char *argv[]) {
+	MainMenu();
+	return 0;
 }
 
-void MainMenu()
-{
+void MainMenu() {
+	while(true) {
+		char selection;
+		
+		cout << "Menu" << endl;
+		cout << "========" << endl;
+		cout << "1 - Load an image for analysis" << endl;
+		cout << "2 - Check for gravitational lensing" << endl;
+		cout << "3 - Save modified image" << endl;
+		cout << "4 - Exit" << endl;
+		cout << "Enter selection: ";
+		
+		cin >> selection;
+		string fileName;
 
-  char selection;
+		switch (selection) {
+			case '1':
+				cout << "Enter image name (with extension): ";
+				cin >> fileName;
+				if(LoadImage()) {
+					cout << "File Loaded!" << endl;
+				}
+				MainMenu();
+				break;
 
-  cout << "\n Menu";
-
-  cout << "\n========";
-
-  cout << "\n 1 - Load an image for analysis";
-
-  cout << "\n 2 - Check for gravitational lensing";
-
-  cout << "\n 3 - Save modified image";
-
-  cout << "\n 4 - Exit";
-
-  cout << "\n Enter selection: ";
-
-  // read the input
-
-  cin >> selection;
-
-  string fileName;
-  switch (selection)
-
-  {
-
-  case '1':
-  {
-    cout << "\n Enter image name + file type: ";
-    cin >> fileName;
-    cout << "\n File Loaded!\n";
-    //LoadImage();
-    MainMenu();
-  }
-
-  case '2':
-  {
-    cout << "\n Running Detection Algorithm....";
-
-    ProcessImage();
-
-    cout << "\n Complete!";
-
-    MainMenu();
-  }
-
-  case '3':
-  {
-    cout << "\n Image Saved To Base Folder";
-
-    //SaveImage();
-    MainMenu();
-  }
-
-  case '4':
-  {
-    cout << "\n Exiting...";
-  }
-  break;
-
-  default:
-    cout << "\n Invalid selection";
-
-    // no break in the default case
-  }
-
-  cout << "\n";
+			case '2':
+				cout << "Running Detection Algorithm...." << endl;
+				ProcessImage();
+				cout << "Complete!" << endl;
+				MainMenu();
+				break;
+			
+			case '3':
+				cout << "\n Image Saved To Base Folder";
+				SaveImage();
+				MainMenu();
+				break;
+			
+			case '4':
+				cout << "\n Exiting...";
+				break;
+			
+			default:
+				cout << "\n Invalid selection";
+		}
+	}
 }
