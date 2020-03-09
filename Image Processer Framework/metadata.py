9f8b2f24691d9aae24d5cddf94bd30ad54e59623
Adityasidher <adityas@nevada.unr.edu>
2020-03-08 23:09:14 -0700

    Added text entry, database, and file load for physics metadata

---
+++ Image Processer Framework/metadata.py
@@ -0,0 +1,24 @@
+
+def physicsMetadata(Star, System, starCluster, Galaxy, galaxyCluster):
+
+    # Holds the name and information for the observed image star in a class struct. 
+    nameOfStar = Star
+    print("Name of Star:", nameOfStar)
+
+    # Holds the name and information for the observed image star in a class struct. 
+    nameOfSystem = System
+    print("Name of System:", nameOfSystem ) 
+
+    # Holds the name and information for the observed star cluster in a class struct. 
+    nameOfStarCluster = starCluster
+    print("Name of Star Cluster:", nameOfStarCluster)
+
+    # Holds the name and information for the observed image in Galaxy in a class struct. 
+    nameOfGalaxy = Galaxy
+    print("Name of Galaxy:", nameOfGalaxy)
+
+    # Holds the name and information for the observed image in a Galaxy Cluster.
+    nameOfGalaxyCluster = galaxyCluster
+    print("Name of Galaxy Cluster:", nameOfGalaxyCluster)
+
+
