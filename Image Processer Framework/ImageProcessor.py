945686e95cb31ac74554b517fbd8ccb343fc8d8e
Ethan Park <ethanpark@nevad.unr.edu>
2020-03-08 18:46:32 -0700

    Added Image Processor

--- Image Processer Framework/ImageProcessor.py
+++ Image Processer Framework/ImageProcessor.py
@@ -18,6 +18,8 @@ from kivy.uix.label import Label
 
 from kivy.uix.button import Button 
 
+from PIL import Image
+import numpy as np
 
 class MetadataEntryPopup(FloatLayout):
     pass
@@ -45,13 +47,23 @@ class MetadataEntryPopup(FloatLayout):
 class Widgets(Widget):
     def dismiss_popup(self):
         self._popup.dismiss()
-#    def Upload_btn(self):
+    def Upload_btn(self):
+        pass
     # UPLOAD IMAGE NOT IMPLEMENTED YET
     def Process_btn(self):
         self.startPopup = ProcessStartPopup(OK=self.dismiss_popup)
         self.startPopup.show()
 
         # IMAGE PROCESSING CODE HERE
+        file_in = "./data_input/lenna_1.png" # change to actual file path
+        image = np.array(Image.open(file_in))
+
+        red = image.copy()
+        red[:, :, (1, 2)] = 0
+
+        pil_img = Image.fromarray(red)
+        file_out = "./data_output/red.png" # change to actual file path
+        pil_img.save(file_out)
 
         self.endPopup = ProcessEndPopup(OK=self.dismiss_popup)
         self.endPopup.show()
@@ -66,6 +78,8 @@ class Widgets(Widget):
 class MyApp(App):
     def build(self):
         return Widgets()
+
+
 
 
 if __name__ == "__main__":
